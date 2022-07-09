e322c72f70147e384df30548b649ab8358707646
inegmetov <negmetov.i@gmail.com>
2022-07-10 00:50:39 +0300

    added db migration

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,26 @@ import (
 	"github.com/inegmetov/todoApp-golang/pkg/handler"
 	"github.com/inegmetov/todoApp-golang/pkg/repository"
 	"github.com/inegmetov/todoApp-golang/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if error := initConfig(); error != nil {
+		log.Fatalf("error initializing config: %v", error.Error())
+	}
+
 	repos := repository.NewRepository()
 	service := service.NewService(repos)
 	handlers := handler.NewHandler(service)
 	srv := new(todoApp.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+
 		log.Fatal("error occurred while running the server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
